b8cc3f0a29bfd3439615319e78535e1a7d6b93e5
Maga <steam.30mb1@gmail.com>
2024-01-07 05:51:29 +0400

    Initial commit

---
+++ contracts/NFT.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.8.20;
+
+import {ERC721Enumerable, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract NFT is ERC721Enumerable, Ownable {
+    struct Round {
+        uint price; // 18 decimals, native USDC
+        uint16 limit; // remaining NFT to mint
+        uint16 offset; // start ID
+        uint8 maxPerUser; // max mints per user
+        bool started;
+    }
+
+    Round[] public rounds;
+    // user => amount of nfts bought for round
+    mapping (address => mapping (uint => uint)) public roundNfts;
+    address public beneficiary;
+    string public baseURI;
+
+    constructor(address _owner, address _beneficiary) ERC721("NFT", "MCO") Ownable(_owner) {
+        beneficiary = _beneficiary;
+
+        rounds.push(Round(3300000000000000000, 5000, 0, 50, false));
+        rounds.push(Round(4400000000000000000, 3000, 5000, 30, false));
+        rounds.push(Round(5500000000000000000, 1000, 8000, 10, false));
+        rounds.push(Round(6600000000000000000, 666, 9000, 6, false));
+        rounds.push(Round(7700000000000000000, 333, 9666, 3, false));
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseUri(string calldata _baseUri) external onlyOwner {
+        baseURI = _baseUri;
+    }
+
+    function setBeneficiary(address _beneficiary) external onlyOwner {
+        beneficiary = _beneficiary;
+    }
+
+    function startRound(uint idx) external onlyOwner {
+        rounds[idx].started = true;
+    }
+
+    function setPrice(uint idx, uint _price) external onlyOwner {
+        rounds[idx].price = _price;
+    }
+
+    function buyNFT(uint idx, uint16 count) external payable {
+        Round memory round = rounds[idx];
+
+        require (idx < rounds.length, "Bad round idx"); // correct round
+        require (round.started, "Round not started"); // round started
+        require (round.limit > count, "Round limit reached"); // enough nft remaining
+        require (msg.value == round.price * count, "Bad value sent"); // correct value sent
+        require (roundNfts[msg.sender][idx] + count <= round.maxPerUser, "User round limit reached"); // user can buy this amount
+
+        // mint to user requested NFTs
+        for (uint i = 0; i < count; i++) {
+            _mint(msg.sender, round.offset + i);
+        }
+        round.offset += count;
+        round.limit -= count;
+        rounds[idx] = round; // save updated roundbuyN
+        roundNfts[msg.sender][idx] += count; // update user round limits
+        payable(beneficiary).transfer(msg.value);
+    }
+}
---
+++ test/1-main.ts
@@ -0,0 +1,118 @@
+import {NFT, NFT__factory} from "../typechain-types";
+import {expect} from "chai";
+import {ethers} from "hardhat";
+
+
+describe("Main test", function () {
+  let nft: NFT;
+
+  it('Deploy', async function() {
+    const [owner, beneficiary] = await ethers.getSigners()
+    nft = await new NFT__factory(owner as any).deploy(owner.address, beneficiary.address);
+    // nft = await ethers.deployContract('NFT', [owner.address, beneficiary.address]);
+
+    const _owner = await nft.owner();
+    expect(_owner).to.be.eq(owner.address);
+
+    const _beneficiary = await nft.beneficiary();
+    expect(_beneficiary).to.be.eq(beneficiary);
+  });
+
+  it('Try buy not started round', async function() {
+    const [owner, beneficiary, buyer] = await ethers.getSigners();
+    await expect(nft.connect(buyer).buyNFT("0", "1")).to.be.revertedWith(
+      "Round not started"
+    );
+  });
+
+  it("Start round", async function() {
+    const [owner, beneficiary, buyer] = await ethers.getSigners();
+
+    await expect(nft.connect(buyer).startRound("0")).to.be.revertedWithCustomError(
+      nft, "OwnableUnauthorizedAccount"
+    );
+
+    await nft.connect(owner).startRound("0");
+  });
+
+  it("User buy nfts", async function() {
+    const [owner, beneficiary, buyer] = await ethers.getSigners();
+
+    // bad value
+
+    await expect(nft.connect(buyer).buyNFT("0", "1")).to.be.revertedWith(
+      "Bad value sent"
+    );
+
+    const tx = await nft.connect(buyer).buyNFT("0", "10", {value: ethers.parseEther("33")});
+    const tx2 = await nft.connect(buyer).buyNFT("0", "10", {value: ethers.parseEther("33")});
+
+    await expect(tx).to.emit(nft, "Transfer");
+
+    const user_bal = await nft.balanceOf(buyer.address);
+    expect(user_bal.toString()).to.be.eq("20");
+
+    await expect(tx).to.changeEtherBalance(beneficiary.address, ethers.parseEther("33"));
+    await expect(tx2).to.changeEtherBalance(beneficiary.address, ethers.parseEther("33"));
+
+    const round = await nft.rounds(0);
+    expect(round.limit.toString()).to.be.eq("4980");
+    expect(round.offset.toString()).to.be.eq("20");
+
+    const user_rounds = await nft.roundNfts(buyer.address, 0);
+    expect(user_rounds.toString()).to.be.eq("20");
+  });
+
+  it("User try buy more than allowed", async function() {
+    const [owner, beneficiary, buyer] = await ethers.getSigners();
+    await expect(nft.connect(buyer).buyNFT("0", "100", {value: ethers.parseEther("330")})).to.be.revertedWith(
+      "User round limit reached"
+    );
+  });
+
+  it("Start next round", async function() {
+    const [owner, beneficiary, buyer] = await ethers.getSigners();
+    await expect(nft.connect(buyer).startRound("1")).to.be.revertedWithCustomError(
+      nft, "OwnableUnauthorizedAccount"
+    );
+    await nft.connect(owner).startRound("1");
+  });
+
+  it("User buy nfts", async function() {
+    const [owner, beneficiary, buyer] = await ethers.getSigners();
+    const tx = await nft.connect(buyer).buyNFT("1", "10", {value: ethers.parseEther("44")});
+    const tx2 = await nft.connect(buyer).buyNFT("1", "10", {value: ethers.parseEther("44")});
+
+    await expect(tx).to.emit(nft, "Transfer");
+
+    const user_bal = await nft.balanceOf(buyer.address);
+    expect(user_bal.toString()).to.be.eq("40");
+
+    await expect(tx).to.changeEtherBalance(beneficiary.address, ethers.parseEther("44"));
+    await expect(tx2).to.changeEtherBalance(beneficiary.address, ethers.parseEther("44"));
+
+    const round = await nft.rounds(1);
+    expect(round.limit.toString()).to.be.eq("2980");
+    expect(round.offset.toString()).to.be.eq("5020");
+
+    const user_rounds = await nft.roundNfts(buyer.address, 1);
+    expect(user_rounds.toString()).to.be.eq("20");
+
+    const new_round_nft_id = await nft.tokenOfOwnerByIndex(buyer.address, 20);
+    expect(new_round_nft_id.toString()).to.be.eq("5000");
+  });
+
+  it('Start last round', async function() {
+    const [owner, beneficiary, buyer] = await ethers.getSigners();
+    await expect(nft.connect(buyer).startRound("4")).to.be.revertedWithCustomError(
+      nft, "OwnableUnauthorizedAccount"
+    );
+    await nft.connect(owner).startRound("4");
+  });
+
+  it('Check round limit', async function() {
+    const [owner, beneficiary, buyer] = await ethers.getSigners();
+    const tx = nft.connect(buyer).buyNFT("4", "500", {value: ethers.parseEther("1000")});
+    await expect(tx).to.be.revertedWith("Round limit reached");
+  });
+});
