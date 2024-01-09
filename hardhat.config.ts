ea6eba1ff4ae493c1ac9092098a460e714a07c1b
Maga <steam.30mb1@gmail.com>
2024-01-09 18:45:02 +0400

    Up

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,21 @@ import '@nomicfoundation/hardhat-chai-matchers'
 
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000
+      }
+    }
+  },
+  networks: {
+    zkfair: {
+      url: "https://rpc.zkfair.io",
+      gasPrice: 5000000000000,
+    },
+  }
 };
 
 export default config;
